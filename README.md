5326a746f077c2e898fa098ab136bacaf95d62be
ddascalescu <djdascalescu@gmail.com>
2022-12-09 10:25:01 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Film ratings app
+A Flutter app for rating films
+
+Dan Dascalescu
---
+++ files/app/android/app/src/main/kotlin/dandascalescu/personal/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package dandascalescu.personal.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
