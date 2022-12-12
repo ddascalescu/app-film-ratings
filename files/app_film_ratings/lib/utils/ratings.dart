21dade064a79329341142f44d09aa8b9549729e1
ddascalescu <djdascalescu@gmail.com>
2022-12-12 19:15:57 +0000

    Added Rating constructor

--- files/app_film_ratings/lib/utils/ratings.dart
+++ files/app_film_ratings/lib/utils/ratings.dart
@@ -1,20 +1,28 @@
+import 'package:intl/intl.dart';
+
+class Ratings {
+  static final DateFormat dateFormat = DateFormat('yyyy-MM-dd');
+}
+
 class Rating {
   // TODO: if started caching films, change this to a corresponding filmID
   String filmTitle;
   int filmYear;
 
   double rating;
-  DateTime ratingDate;
+  DateTime? ratingDate;
 
-  Rating(this.filmTitle, this.filmYear, this.rating, this.ratingDate);
+  Rating(this.filmTitle, this.filmYear, this.rating, DateTime ratingDate) {
+    this.ratingDate = DateTime(ratingDate.year, ratingDate.month, ratingDate.day);
+  }
 
   @override
   String toString() {
     return
       'Rating {\n'
-        '\tFilm: ${filmTitle} (${filmYear})\n'
-        '\tRating: ${rating}\n'
-        '\tRating date: ${ratingDate}\n'
+        '\tFilm: $filmTitle ($filmYear)\n'
+        '\tRating: $rating\n'
+        '\tRating date: ${Ratings.dateFormat.format(ratingDate!)}\n'
       '}';
   }
 }
