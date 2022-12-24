535a26c4b5c985ef94f03b478efaf41a44358644
ddascalescu <djdascalescu@gmail.com>
2022-12-24 18:42:26 +0000

    Changed Indigo.swatch() method to Indigo.swatch field

--- files/app_film_ratings/lib/main.dart
+++ files/app_film_ratings/lib/main.dart
@@ -24,7 +24,7 @@ class App extends StatelessWidget {
     return MaterialApp(
         title: appTitle,
         theme: ThemeData(
-          primarySwatch: Indigo.swatch()
+          primarySwatch: Indigo.swatch
         ),
         home: const MainScreen()
     );
